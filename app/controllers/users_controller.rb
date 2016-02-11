b02a69e19473541c479e4bf879e7d0acf8ad9aea
railsbeginner <bartosz.dyja@gmail.com>
2016-02-11 13:31:14 +0100

    Add user sign up feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, flash: {notice: 'User successfully created'}
+    else
+      flash[:error] = 'Cannot save user'
+      render 'new'
+    end
+  end
+
+  def index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
+  end
+end
