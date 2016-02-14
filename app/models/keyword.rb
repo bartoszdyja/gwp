c8f071415682b396621f286cc8677ea25bac99bc
railsbeginner <bartosz.dyja@gmail.com>
2016-02-14 12:23:00 +0100

    Add google custom search engine

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -4,14 +4,22 @@ class Keyword < ActiveRecord::Base
 
   after_save :check_position
 
-  private
+  # private
 
   def check_position
-    positions.create(rank: hit_google)
+    positions.create(rank: hit_google(website.url, phrase))
   end
 
-  def hit_google
-    conn = Faraday.get('http://www.wp.pl')
-    conn.status
+  def hit_google(website, keyword)
+    conn = Faraday.get("https://www.googleapis.com/customsearch/v1?q=#{keyword}&cx=#{Rails.application.secrets.cx}&key=#{Rails.application.secrets.secret_google_key}")
+    p conn.status
+    if conn.status == 200
+      response = JSON.parse(conn.body)['items']
+      return 'Error! Nothing found' if response.blank?
+      p links = response.map {|item| URI.parse(item['link']).host}
+      links.find_index(URI.parse(website).host) || 'Not in the first 10'
+    else
+      'Please try again later'
+    end
   end
 end
